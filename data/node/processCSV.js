73c3b470bead7665755992d34cdb8ca8dcc08796
Dario Segura <dario.seco@gmail.com>
2019-02-06 23:09:20 -0500

    [DATA] Processing refactored + beans file format

--- data/node/processCSV.js
+++ data/node/processCSV.js
@@ -1,14 +1,19 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-const path = require('path');
 
 function processCSV(file, options, cb) {
     const csvOptions = Object.assign({}, options);
     return new Promise((resolve, reject) => {
-        const stream = fs.createReadStream(path.resolve(__dirname, file))
+        let totalBytesRead = 0;
+        const stream = fs.createReadStream(file)
             .pipe(csv(csvOptions))
-            .on('data', data => {
-                cb(data);
+            .on('headers', data => {
+                totalBytesRead += stream._currentRowBytes;
+                cb(null, data, totalBytesRead);
+            })
+            .on('data', (data) => {
+                totalBytesRead += stream._currentRowBytes;
+                cb(data, null, totalBytesRead);
             }).on('end', () => {
                 stream.destroy();
             }).on('error', e => {
